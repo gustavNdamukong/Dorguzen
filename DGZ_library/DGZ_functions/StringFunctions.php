6bc6481da79340e2c33f4d4e11bc60b643cb7910
Gustav <gustavfn@yahoo.co.uk>
2020-11-05 22:00:41 +0000

    added description to string functions class

--- DGZ_library/DGZ_functions/StringFunctions.php
+++ DGZ_library/DGZ_functions/StringFunctions.php
@@ -3,23 +3,22 @@
 namespace DGZ_library\DGZ_functions;
 
 /**
- * Description of StringFunctions
- *
- * @author vasicj
- */
+* Description of StringFunctions
+*
+* @author Gustav
 class StringFunctions {
-	
-	/**
-	 * @description standard startsWidth function that doesn't seem to exist in php for some reason
-	 * @usage  if (/Utils/StringFunctions::startsWith($this->model->salesOrder, 'IBT')==true)<br>
-				   return "is IBT";
-	 * @param string $haystack
-	 * @param string $needle
-	 * @return boolean
-	 */
-	public static function startsWith($haystack, $needle) {
-		return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
-	}
-	
-	
+
+/**
+* @description standard startsWidth function that doesn't seem to exist in php for some reason
+* @usage  if (/StringFunctions::startsWith($this->getSellerType(), 'Bus')==true)<br>
+return "is a business";
+* @param string $haystack
+* @param string $needle
+* @return boolean
+*/
+public static function startsWith($haystack, $needle) {
+return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
+}
+
+
 }
